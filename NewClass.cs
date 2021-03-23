3b9e565ff96318420ea2a7e18a34032a3a34a450
student <Student@B420-01.fesb.hr>
2021-03-23 18:44:13 +0100

    Added new class

---
+++ NewClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prvi.c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Novo");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
