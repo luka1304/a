65aeae9ac9e643a12558d16212027b507cc46326
student <Student@B420-01.fesb.hr>
2021-03-23 18:24:46 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prvi.c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
